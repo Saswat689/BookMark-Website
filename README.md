40e4c0ed17600a8544d2b46af675c19b218a3219
Saswat Singh <saswatsingh629@gmail.com>
2021-12-10 20:31:10 +0530

    added all folders

---
+++ README.md
@@ -0,0 +1,19 @@
+## BookMark Website UI built with Tailwind CSS
+
+A responsive website that uses tailwind and a couple of more libraries under the hood to make your experience awesome.
+
+## Previews
+
+
+
+## Demo
+
+Check the site live :fire: 
+
+## Features
+
+- Reveal on scroll animations (wow.js && animate.css)
+- Completely responsive
+- Only HTML design (Tailwind css)
+- Navigation
+- Forms
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "bookmark-purple": "#5267Df",
+        "bookmark-grey": "#FA5959",
+        "bookmark-blue": "#242A45",
+        "bookmark-white": "#F7F7F7",
+      },
+    },
+    fontFamily: {
+      Poppins: ["Poppins,sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1224px",
+        xl: "1224px",
+        "2xl": "1224px",
+      }    
+    }
+  },
+  plugins: [],
+}
