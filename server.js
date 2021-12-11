8ecbc39196f0a3a226819f68b6b34a994eb3a516
Saswat Singh <saswatsingh629@gmail.com>
2021-12-11 09:20:40 +0530

    added static routes

--- server.js
+++ server.js
@@ -3,8 +3,14 @@
 const app = require("express")();
 const path = require("path");
 
+//static
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+//all routes
 app.get("/*",(req,res) => {
 	res.sendFile(path.join(__dirname, './public', 'index.html'));
 })
 
+
+//listen on production port
 app.listen(process.env.PORT || 3000,() => {console.log("Server active")})
